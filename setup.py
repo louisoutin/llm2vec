b1bf13234ac9591b2741795efb9a281133bf6a69
Louis <louis.outin@gmail.com>
2025-07-26 09:52:06 +0100

    Update setup.py
    Remove transformers version constraints

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "tqdm",
         "torch",
         "peft",
-        "transformers>=4.39.1,<=4.40.2",
+        "transformers",
         "datasets",
         "evaluate",
         "scikit-learn",
